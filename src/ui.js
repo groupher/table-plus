e5ffe7b2a5e11e8e594605f38b8946c11f5e55bc
mydearxym <mydearxym@qq.com>
2021-01-05 16:04:21 +0800

    chore: basic row handler ux

--- src/ui.js
+++ src/ui.js
@@ -2,7 +2,7 @@
 import ajax from "@codexteam/ajax";
 // eslint-disable-next-line
 import polyfill from "url-polyfill";
-import { make, findIndex } from '@groupher/editor-utils';
+import { make, findIndex } from "@groupher/editor-utils";
 
 /**
  * @description the ui parts
@@ -21,19 +21,19 @@ export default class UI {
      * Tool's initial config
      */
     this.config = {
-      endpoint: config.endpoint || ''
+      endpoint: config.endpoint || "",
     };
 
     this.nodes = {
       // root element
       wrapper: null,
       container: null,
-      defaultTable: null
+      defaultTable: null,
     };
 
     this._data = {
-      link: '',
-      meta: {}
+      link: "",
+      meta: {},
     };
 
     // this.data = data;
@@ -44,33 +44,46 @@ export default class UI {
       columnCount: 3,
       items: [
         {
-          text: 'cell 0',
-          index: 0
+          text: "cell 0",
+          index: 0,
           // align: '..',
         },
         {
-          text: 'cell 1',
-          index: 1
+          text: "cell 1",
+          index: 1,
         },
         {
-          text: 'cell 2',
-          index: 2
+          text: "cell 2",
+          index: 2,
         },
         {
-          text: 'cell 3',
-          index: 3
+          text: "cell 3",
+          index: 3,
         },
         {
-          text: 'cell 4',
-          index: 4
+          text: "cell 4",
+          index: 4,
         },
         {
-          text: 'cell 5',
-          index: 5
-        }
-      ]
+          text: "cell 5",
+          index: 5,
+        },
+        {
+          text: "cell 6",
+          index: 6,
+        },
+        {
+          text: "cell 7",
+          index: 7,
+        },
+        {
+          text: "cell 8",
+          index: 8,
+        },
+      ],
     };
 
+    this.columnHandlers = [];
     this.rowHandlers = [];
   }
 
@@ -86,10 +99,11 @@ export default class UI {
       /**
        * Tool's classes
        */
-      container: 'cdx-table-wrapper',
-      table: 'cdx-table',
-      cell: 'cdx-table__cell',
-      handler: 'cdx-table__handler'
+      container: "cdx-table-wrapper",
+      table: "cdx-table",
+      cell: "cdx-table__cell",
+      columnHandler: "cdx-table__column_handler",
+      rowHandler: "cdx-table__row_handler",
     };
   }
 
@@ -97,8 +111,8 @@ export default class UI {
    * draw render View
    */
   drawView() {
-    const wrapperEl = make('div', this.CSS.baseClass);
-    const containerEl = make('div', this.CSS.container);
+    const wrapperEl = make("div", this.CSS.baseClass);
+    const containerEl = make("div", this.CSS.container);
 
     this.nodes.defaultTable = this._drawDefaultTable();
 
@@ -112,8 +126,8 @@ export default class UI {
    * draw table element
    */
   _drawDefaultTable() {
-    const TableEl = make('table', this.CSS.table);
-    const TBodyEl = make('tbody');
+    const TableEl = make("table", this.CSS.table);
+    const TBodyEl = make("tbody");
 
     const { columnCount, items } = this.data;
 
@@ -134,7 +148,7 @@ export default class UI {
    * @memberof UI
    */
   _drawRow(items) {
-    const RowEl = make('tr');
+    const RowEl = make("tr");
 
     items.forEach((item) => {
       RowEl.appendChild(this._drawCell(item));
@@ -149,25 +163,33 @@ export default class UI {
    * @memberof UI
    */
   _drawCell(item) {
-    const TdEl = make('td');
-    const CellEl = make('div', this.CSS.cell, {
+    const TdEl = make("td");
+    const CellEl = make("div", this.CSS.cell, {
       innerHTML: item.text,
       contentEditable: true,
-      'data-index': item.index
+      "data-index": item.index,
     });
 
     TdEl.appendChild(CellEl);
 
     if (item.index < this.data.columnCount) {
-      const RowHandlerEl = this._drawRowSettingHandler(item);
+      const HandlerEl = this._drawColumnSettingHandler(item);
 
-      TdEl.appendChild(RowHandlerEl);
-      this.rowHandlers.push(RowHandlerEl);
+      TdEl.appendChild(HandlerEl);
+      this.columnHandlers.push(HandlerEl);
     }
 
-    TdEl.addEventListener('mouseover', ({ target: { dataset } }) => {
+    if (item.index % this.data.columnCount === 0) {
+      const HandlerEl = this._drawRowSettingHandler(item);
+
+      TdEl.appendChild(HandlerEl);
+      this.rowHandlers.push(HandlerEl);
+    }
+
+    TdEl.addEventListener("click", ({ target: { dataset } }) => {
       if (dataset.index) {
         this._showColumnHandler(dataset.index);
+        this._showRowHandler(dataset.index);
       }
     });
 
@@ -176,7 +198,8 @@ export default class UI {
 
   /**
    * judge column by given index
-   *
+   * @param {Number} index
+   * @return {Number}
    * @memberof UI
    */
   _whichColumn(index) {
@@ -186,41 +209,98 @@ export default class UI {
   }
 
   /**
+   * judge row by given index
+   * @param {Number} index
+   * @return {Number}
+   * @memberof UI
+   */
+  _whichRow(index) {
+    const { columnCount } = this.data;
+
+    return Math.floor(parseInt(index) / columnCount);
+  }
+
+  /**
    *
    * @param {Number} index
    * @memberof UI
    */
   _showColumnHandler(index) {
     const columnIndex = this._whichColumn(index);
+    const handlerEls = this.columnHandlers;
 
-    const targetIndex = findIndex(this.rowHandlers, (item) => {
+    const targetIndex = findIndex(handlerEls, (item) => {
       return parseInt(item.dataset.columnIndex) === columnIndex;
     });
 
     if (targetIndex >= 0) {
-      for (let i = 0; i < this.rowHandlers.length; i += 1) {
-        const handlerEl = this.rowHandlers[i];
+      for (let i = 0; i < handlerEls.length; i += 1) {
+        const handlerEl = handlerEls[i];
 
         handlerEl.style.opacity = 0;
       }
-      this.rowHandlers[targetIndex].style.opacity = 1;
+      handlerEls[targetIndex].style.opacity = 1;
     }
 
     return targetIndex;
   }
 
   /**
-   * draw handler
+   *
+   * @param {Number} index
+   * @memberof UI
+   */
+  _showRowHandler(index) {
+    const rowIndex = this._whichRow(index);
+    const handlerEls = this.rowHandlers;
+
+    // console.log('# rowIndex: ', rowIndex);
+    // console.log('# row handlerEls: ', handlerEls);
+    const targetIndex = findIndex(handlerEls, (item) => {
+      // console.log('#> each rowIndex: ', item.dataset.rowIndex);
+      return parseInt(item.dataset.rowIndex) === rowIndex;
+    });
+
+    // console.log('# targetIndex: ', targetIndex);
+
+    if (targetIndex >= 0) {
+      for (let i = 0; i < handlerEls.length; i += 1) {
+        const handlerEl = handlerEls[i];
+
+        handlerEl.style.opacity = 0;
+      }
+      handlerEls[targetIndex].style.opacity = 1;
+    }
+
+    return targetIndex;
+  }
+
+  /**
+   * draw column handler
    *
    * @memberof UI
    */
-  _drawRowSettingHandler(item) {
-    const HandlerEl = make('div', this.CSS.handler, {
-      'data-column-index': this._whichColumn(item.index)
+  _drawColumnSettingHandler(item) {
+    const HandlerEl = make("div", this.CSS.columnHandler, {
+      "data-column-index": this._whichColumn(item.index),
     });
 
     return HandlerEl;
   }
+
+  /**
+   * draw raw handler
+   *
+   * @memberof UI
+   */
+  _drawRowSettingHandler(item) {
+    const HandlerEl = make("div", this.CSS.rowHandler, {
+      "data-row-index": this._whichRow(item.index),
+    });
+
+    return HandlerEl;
+  }
+
   // _highlightRow () {}
   // _highlightColumn () {}
 }
